0e8e3c1c70147830e029500ac32e3c8eabc563f7
DongDongShin <tlsehdgk87@naver.com>
2022-09-24 01:08:54 +0900

    add: dto에서 page리턴 method구현

--- src/main/java/com/example/demo/dto/response/BookResponseDto.java
+++ src/main/java/com/example/demo/dto/response/BookResponseDto.java
@@ -1,11 +1,17 @@
 package com.example.demo.dto.response;
 
 
+import com.example.demo.domain.Book;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.domain.Page;
 
-@Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BookResponseDto {
     private Long id;
     private String title;
@@ -14,8 +20,19 @@ public class BookResponseDto {
 
     private String publisher;
 
-
-
     private String isbn;
     private String library;
+
+    public  Page<BookResponseDto> toDtoList(Page<Book> postList){
+        Page<BookResponseDto> ResponsePostList = postList.map(m -> BookResponseDto.builder()
+                .id(m.getId())
+                .title(m.getBookName())
+                .writer(m.getAuthors())
+                .publisher(m.getPublisher())
+                .isbn(m.getIsbn13())
+                .library(m.getLibraryName())
+                .build()
+        );
+        return ResponsePostList;
+    }
 }
