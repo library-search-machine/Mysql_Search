3f21eb222290faf9f8502683dbe11dad1099eb58
DongDongShin <tlsehdgk87@naver.com>
2022-09-24 01:09:33 +0900

    add: repository에서 page 객체 받음

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -6,6 +6,10 @@ import com.example.demo.dto.response.BookResponseDto;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,34 +23,25 @@ public class BookService {
     private final BookRepository bookRepository;
 
     @Transactional
-    public List<BookResponseDto> getBook(String keyword, String type) {
-        List<Book> bookList;
+    public Page<BookResponseDto> getBook(String keyword, String type,int page) {
+        Pageable pageable = PageRequest.of(page - 1, 30);
+        Page<Book> bookList;
         long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
         if (type.equals("title"))
-            bookList = bookRepository.findByBookNameContaining(keyword);
+            bookList = bookRepository.findByBookNameContaining(keyword,pageable);
 
         else if (type.equals("writer"))
-            bookList = bookRepository.findByAuthorsContaining(keyword);
+            bookList = bookRepository.findByAuthorsContaining(keyword,pageable);
         else
-            bookList = bookRepository.findByIsbn13Containing(keyword);
-        System.out.println(bookList.size());
-        List<BookResponseDto> bookResponseDtoList = new ArrayList<>();
-        for (Book book : bookList) {
-            bookResponseDtoList.add(
-                    BookResponseDto.builder()
-                            .id(book.getId())
-                            .title(book.getBookName())
-                            .writer(book.getAuthors())
-                            .isbn(book.getIsbn13())
-                            .library(book.getLibraryName())
-                            .build()
-            );
-        }
+            bookList = bookRepository.findByIsbn13Containing(keyword,pageable);
+        Page<BookResponseDto> bookResponseDtoList = new BookResponseDto().toDtoList(bookList);
+
         long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
         long secDiffTime = (afterTime - beforeTime);
-        System.out.println("시간차이(m) : " + secDiffTime);
+        System.out.println("시간차이(m) : using full text search" + secDiffTime);
         return bookResponseDtoList;
     }
+
 
 
 }
