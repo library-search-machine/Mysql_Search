844100d288c0dc2c902b5790baf9276a6091a32b
DongDongShin <tlsehdgk87@naver.com>
2022-09-25 19:53:52 +0900

    feat: isbn으로 도서나루 api에 접근하여 해당 책의 정보를 받는 기능추가

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -3,17 +3,25 @@ package com.example.demo.service;
 
 import com.example.demo.domain.Book;
 import com.example.demo.dto.response.BookResponseDto;
+import com.example.demo.dto.response.BookResponseDto2;
 import com.example.demo.repository.BookRepository;
-import com.example.demo.response.ResponseDto;
 import lombok.RequiredArgsConstructor;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 @Service
@@ -24,6 +32,7 @@ public class BookService {
 
     @Transactional
     public Page<BookResponseDto> getBook(String keyword, String type,int page) {
+
         Pageable pageable = PageRequest.of(page - 1, 30);
         Page<Book> bookList;
         long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
@@ -40,6 +49,55 @@ public class BookService {
         long secDiffTime = (afterTime - beforeTime);
         System.out.println("시간차이(m) : using full text search" + secDiffTime);
         return bookResponseDtoList;
+    }
+
+
+    @Transactional
+    public BookResponseDto2 getBookByIsbn(String isbn) throws MalformedURLException {
+        List<String> LibraryList = bookRepository.findByLibraryName(isbn);//
+        String url_address = "http://data4library.kr/api/srchDtlList?authKey=6bd363e870bb744d2e52c35f15cfef0aa929faba70bc2d66961aae91e101901f&isbn13="+isbn+"&loaninfoYN=N&format=json";
+        try{
+            URL url = new URL(url_address);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Content-type", "application/json");
+            BufferedReader rd;
+            if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
+                rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            } else {
+                rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+            }
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = rd.readLine()) != null) {
+                sb.append(line);
+            }
+            rd.close();
+            conn.disconnect();
+            JSONParser parser	= new JSONParser();
+            JSONObject obj =(JSONObject)parser.parse(sb.toString());
+            JSONObject response =(JSONObject)obj.get("response");
+            JSONArray detail =(JSONArray) response.get("detail");
+            JSONObject temp = (JSONObject)detail.get(0);
+            JSONObject book = (JSONObject)temp.get("book");
+
+
+            BookResponseDto2 bookResponseDto = BookResponseDto2.builder()
+                    .bookName((String)book.get("bookname"))
+                    .authors((String)book.get("authors"))
+                    .publisher((String)book.get("publisher"))
+                    .class_nm((String)book.get("class_nm"))
+                    .publicationYear((String)book.get("publication_year"))
+                    .bookImageURL((String)book.get("bookImageURL"))
+                    .description((String)book.get("description"))
+                    .LibraryList(LibraryList)
+                    .build();
+            return  bookResponseDto;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return null;
     }
 
 
