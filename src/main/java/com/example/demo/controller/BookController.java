07ac450c5cec0dfe72de1853ace5f6ea352670e6
DongDongShin <tlsehdgk87@naver.com>
2022-09-25 19:51:56 +0900

    feat: 페이징 기능보완, 세부 페이지 추가

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -5,6 +5,7 @@ package com.example.demo.controller;
 import com.example.demo.domain.Book;
 import com.example.demo.dto.response.BookResponseDto;
 
+import com.example.demo.dto.response.BookResponseDto2;
 import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.MalformedURLException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -26,31 +28,63 @@ public class BookController {
 
     @GetMapping("/")
     public String main(Model model){
-
         return "main";
     }
-
-
-
     @GetMapping("/search")
     public String getBook(@RequestParam() String keyword,@RequestParam() String type,@RequestParam() int page,Model model){
         Page<BookResponseDto> bookList =  bookService.getBook(keyword,type,page);
         model.addAttribute("totalPages", bookList.getTotalPages());
         model.addAttribute("totalItems", bookList.getTotalElements());
-
+        model.addAttribute("keyword", keyword);
+        model.addAttribute("type", type);
+        model.addAttribute("current_page", page);
+        int startIndex;
+        int endIndex;
         long startCount = (page - 1) * 30 + 1;
-        model.addAttribute("startCount", startCount);
-
         long endCount = startCount + 30 - 1;
-        if (endCount > bookList.getTotalElements()) {
-            endCount = bookList.getTotalElements();
+        model.addAttribute("startCount", startCount);
+        model.addAttribute("endCount", endCount);
+        if (page / 10 < 1) {
+            startIndex=1;
+            System.out.println("startIndex = " + startIndex);
+            endIndex=10;
+            if(endIndex>=bookList.getTotalPages())
+                endIndex= bookList.getTotalPages();
+        } else  {
+            if(page%10==0){
+                page-=1;
+            }
+            System.out.println("page = " + page);
+            startIndex=page/10*10+1;
+            endIndex=startIndex+9;
+            if(endIndex>=bookList.getTotalPages())
+                endIndex= bookList.getTotalPages();
         }
 
-        model.addAttribute("endCount", endCount);
+        model.addAttribute("startIndex", startIndex);
+        model.addAttribute("endIndex", endIndex);
+
+
+        //지금 필요한거 지금 total pae
+
         model.addAttribute("bookList", bookList);
         model.addAttribute("keyword", keyword);
 
         return "main";
+    }
+    @GetMapping("/search_isbn")
+    public String getBookIsbn(@RequestParam() String isbn,Model model) throws MalformedURLException {
+        //isbn으로 호출을하고 그 책들의 정보들을 호출해 주고 그리고 또 뭐냐 full text index써서 해당 도서관리스트를 리턴을 하자
+        BookResponseDto2 bookByIsbn = bookService.getBookByIsbn(isbn);
+        model.addAttribute("bookname",bookByIsbn.getBookName());
+        model.addAttribute("authors",bookByIsbn.getAuthors());
+        model.addAttribute("publisher",bookByIsbn.getPublisher());
+        model.addAttribute("publicationYear",bookByIsbn.getPublicationYear());
+        model.addAttribute("bookImageURL",bookByIsbn.getBookImageURL());
+        model.addAttribute("description",bookByIsbn.getDescription());
+        model.addAttribute("class_nm",bookByIsbn.getClass_nm());
+        model.addAttribute("LibraryList",bookByIsbn.getLibraryList());
+        return "detail";
     }
 
 
